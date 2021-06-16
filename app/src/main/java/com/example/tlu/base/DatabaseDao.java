31d372d93b8462252e07118119ad85109f5f322d
Andres Pihl <ap123@tlu.ee>
2021-06-16 13:07:29 +0300

    lookForRoom query

--- app/src/main/java/com/example/tlu/base/DatabaseDao.java
+++ app/src/main/java/com/example/tlu/base/DatabaseDao.java
@@ -10,6 +10,7 @@ public interface DatabaseDao {
     @Query("SELECT * FROM nav_points WHERE room_id IS :roomID")
     List<NavPoints> findDestination(int roomID);
 
-
+    @Query("SELECT * FROM room INNER JOIN room_name ON room.room_name_id = room_name.id WHERE code LIKE :search OR ee LIKE :search OR en LIKE :search")
+    List<Room> lookForRoom(String search);
 
 }
