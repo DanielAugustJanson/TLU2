f2e1c3e9d385f2f217627e55b1b4addabccad6c8
Andres Pihl <ap123@tlu.ee>
2021-06-17 11:14:14 +0300

    Find Neighbour, Find Destination

--- app/src/main/java/com/example/tlu/MainActivity.java
+++ app/src/main/java/com/example/tlu/MainActivity.java
@@ -30,11 +30,13 @@ public class MainActivity extends AppCompatActivity {
     //Creating a new instance of MapView Object.
     private MapView mapView;
     public static AppiDatabase db;
-    private final ThreadPoolExecutor appiExecute = new ThreadPoolExecutor(2, 4, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+    private final ThreadPoolExecutor appiExecute = new ThreadPoolExecutor(2, 4, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     public static List<Room> forPoints;
     public static List<Room> roomsForLookUp;
     public static int floorDisplayed = 1;
+    public static int roomIDForDestination;
     public static String searchTerm;
+    public static List<NavPoints> destinationHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
