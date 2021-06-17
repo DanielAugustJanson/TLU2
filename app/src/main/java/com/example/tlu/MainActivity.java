307f338bd33bbaa2d52528bfd20783b0719fdd6b
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2021-06-17 20:45:09 +0300

    Korrustevahetusolemas

--- app/src/main/java/com/example/tlu/MainActivity.java
+++ app/src/main/java/com/example/tlu/MainActivity.java
@@ -27,7 +27,9 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.Layer;
-import com.mapbox.mapboxsdk.style.layers.RasterLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Locale;
@@ -35,10 +37,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import static com.mapbox.mapboxsdk.style.layers.Property.NONE;
-import static com.mapbox.mapboxsdk.style.layers.Property.VISIBLE;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleRadius;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.rasterOpacity;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.visibility;
 
 
@@ -46,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Creating a new instance of MapView Object.
     private MapView mapView;
+    private MapboxMap mapboxMap;
     public static AppiDatabase db;
     private final ThreadPoolExecutor appiExecute = new ThreadPoolExecutor(2, 4, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     public static List<Room> forPoints;
@@ -55,21 +55,29 @@ public class MainActivity extends AppCompatActivity {
     public static String searchTerm;
     public static List<NavPoints> destinationHolder;
 
+    public Layer astra1K;
+    public Layer astra1AK;
+    public Layer astra2K;
+    public Button buttonA1A;
+    public Button buttonA1;
+    public Button buttonA2;
+
+
     AutoCompleteTextView autoCompleteTextView;
     Button calendar;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-
-
         db = androidx.room.Room.databaseBuilder(getApplicationContext(),
                 AppiDatabase.class, "appi-data.db").createFromAsset("database/appi.db").fallbackToDestructiveMigration().build();
 
+
         setContentView(R.layout.activity_main);
+
+
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(getResources().getString(R.string.app_name2));
@@ -82,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         appiExecute.execute(new GetRoomPoints());
         //appiExecute.execute(new LookForRoomByCode());
 
@@ -90,43 +99,62 @@ public class MainActivity extends AppCompatActivity {
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                MainActivity.this.mapboxMap = mapboxMap;
                 mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/gustavjohannson/ckpzb9eop05mq18qviptlbm5d"), new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
 
                         Layer background = style.getLayer("background");
                         Layer groundFloor = style.getLayer("Base");
+                        //
                         Layer astra1K = style.getLayer("Astra-1K");
                         Layer astra1AK = style.getLayer("Astra-1AK");
+                        Layer astra2K = style.getLayer("Astra-2K");
 
 
-                        //Seadistame esialgse kaamera positsiooni
+                        //Seadistame esialgse kaamera positsiooni - Suurt vahet pole, niigi ei tööta.
                         CameraPosition startingCameraPosition = new CameraPosition.Builder()
                                 .target(new LatLng(0.3177,-0.2487))
-                                .zoom(12)
+                                .zoom(10)
                                 .build();
 
                         mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(startingCameraPosition),100);
 
+                        Button buttonA1A = findViewById(R.id.floor1a);
+                        Button buttonA1 = findViewById(R.id.floor1);
+                        Button buttonA2 = findViewById(R.id.floor2);
 
+                        buttonA1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ChangeLayer("A1");
 
-                        // Custom map style has been loaded and map is now ready
+                            }
+                        });
+                        buttonA1A.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ChangeLayer("A1A");
 
+                            }
+                        });
+                        buttonA2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ChangeLayer("A2");
 
+                            }
+                        });
                     }
                 });
-
-
-
             }
         });
-
         autoCompleteTextView = findViewById(R.id.StartPoint);
-
-
     }
 
     // Põmst peame iga activity funktsiooni alla kirjutama et Map teeks samaaegselt samu ajsu.
+
+
 
 
     @Override
@@ -221,6 +249,34 @@ public class MainActivity extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         Resources resources = new Resources(getAssets(), metrics, conf);
         return resources.getString(id);
+    }
+    public void ChangeLayer(String floornumber){
+        mapboxMap.getStyle(new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull @NotNull Style style) {
+                Layer astra1K = style.getLayer("Astra-1K");
+                Layer astra1AK = style.getLayer("Astra-1AK");
+                Layer astra2K = style.getLayer("Astra-2K");
+
+                switch (floornumber){
+                    case "A1":
+                        astra1K.setProperties(visibility(Property.VISIBLE));
+                        astra1AK.setProperties(visibility(Property.NONE));
+                        astra2K.setProperties(visibility(Property.NONE));
+                        break;
+                    case "A1A":
+                        astra1K.setProperties(visibility(Property.NONE));
+                        astra1AK.setProperties(visibility(Property.VISIBLE));
+                        astra2K.setProperties(visibility(Property.NONE));
+                        break;
+                    case "A2":
+                        astra1K.setProperties(visibility(Property.NONE));
+                        astra1AK.setProperties(visibility(Property.NONE));
+                        astra2K.setProperties(visibility(Property.VISIBLE));
+                }
+
+            }
+        });
     }
 
 }
